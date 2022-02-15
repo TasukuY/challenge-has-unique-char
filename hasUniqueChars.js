2dee4db663fae0b91221b33d9885028d884e16ae
TYama <alohaty81@gmail.com>
2022-02-14 21:16:15 -0700

    hasUniqueChars done

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,22 @@
 // Write your code below
+
+let hasUniqueChars = (str) => {
+    let hasUniqueChar = false;
+    //split the str into an array
+    let strArr = str.split('');
+    //check each char and check if there is the same char in the array
+    console.log(strArr);
+    for(let i = 0; i < strArr.length; i++){
+        //first letter to check
+        let letterToCheck = strArr[i];
+        for(let j = i+1; j < strArr.length; j++){
+            if(letterToCheck === strArr[j]){
+                hasUniqueChar = true
+            }
+        }
+    }
+
+    return hasUniqueChar;
+}
+
+console.log(hasUniqueChars('abcdefghijka'));
