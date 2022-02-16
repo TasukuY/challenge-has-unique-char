49cf810f981d5891a48f5dde051ed21127b138b9
TYama <alohaty81@gmail.com>
2022-02-16 10:16:32 -0700

    made another solution

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -5,7 +5,7 @@ let hasUniqueChars = (str) => {
     //split the str into an array
     let strArr = str.split('');
     //check each char and check if there is the same char in the array
-    console.log(strArr);
+    //console.log(strArr);
     for(let i = 0; i < strArr.length; i++){
         //first letter to check
         let letterToCheck = strArr[i];
@@ -20,7 +20,16 @@ let hasUniqueChars = (str) => {
 
 console.log(hasUniqueChars('Moonday'));
 
-// let hasUniqueChars2 = (str) => {
-//     let hasUniqueChar = false;
+let hasUniqueChars2 = (str) => {
+    let hasUniqueChar = false
+    const newStr = new Set(str);
+    let newArr = [...newStr];
+    
+    if(str.length !== newArr.length){
+        return false;
+    }else{
+        return true;
+    }
+}
 
-// }
+console.log(hasUniqueChars2('Moonday'));
